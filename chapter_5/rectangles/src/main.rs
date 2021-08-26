090722441e0f321d9ffd93aa58f868546b4a212b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-08-26 16:30:26 +0900

    clean

--- chapter_5/rectangles/src/main.rs
+++ chapter_5/rectangles/src/main.rs
@@ -1,13 +1,22 @@
-fn main() {
-    let width1 = 30;
-    let height1 = 50;
-
-    println!(
-        "長方形の面積は、{} 平方ピクセルです！",
-        area(width1, height1)
-    );
+// デバッグ出力を利用したいので明示的に指定
+#[derive(Debug)]
+struct Rect {
+    width: u32,
+    height: u32,
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn main() {
+    let rect1 = Rect {
+        width: 30,
+        height: 50,
+    };
+
+    println!("長方形の面積は、{} 平方ピクセルです！", area(&rect1));
+
+    // デバッグ用に全フィールドの値を表示する
+    println!("{:#?}", rect1);
+}
+
+fn area(rect: &Rect) -> u32 {
+    rect.width * rect.height
 }
