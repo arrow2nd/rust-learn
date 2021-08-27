e947ab0e1c65ccedeef337fc7772c7515125454c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-08-27 21:58:59 +0900

    add: method

--- chapter_5/rectangles/src/main.rs
+++ chapter_5/rectangles/src/main.rs
@@ -5,18 +5,41 @@ struct Rect {
     height: u32,
 }
 
+// 構造体上にメソッドを定義（Goっぽい）
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rect) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // 関連関数（selfを引数に取らない）
+    fn square(size: u32) -> Rect {
+        Rect {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rect {
         width: 30,
         height: 50,
     };
+    let rect2 = Rect {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rect::square(12);
 
-    println!("長方形の面積は、{} 平方ピクセルです！", area(&rect1));
+    println!("rect1の面積は、{} 平方ピクセルです！", rect1.area());
+
+    println!("rect1 > rect2 : {}", rect1.can_hold(&rect2));
+    println!("rect1 > rect3 : {}", rect1.can_hold(&rect3));
 
     // デバッグ用に全フィールドの値を表示する
-    println!("{:#?}", rect1);
-}
-
-fn area(rect: &Rect) -> u32 {
-    rect.width * rect.height
+    // println!("{:#?}", rect1);
 }
