7cefe9e495728efcd7ac3b1c40f1b93606ebf248
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-08-28 13:41:21 +0900

    add: match

---
+++ chapter_6/match_flow/src/main.rs
@@ -0,0 +1,48 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("six = {:?}, none = {:?}", six, none);
+
+    let some_u8_val = 0u8;
+    match some_u8_val {
+        1 => println!("one"),
+        7 => println!("seven"),
+        _ => (), // 全ての値にマッチ（ここでは何もしない）
+    }
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("state quarter from {:?}", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
