165ae372b31fbca8de9065e20aefa8c32fcaa560
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-08-30 11:09:49 +0900

    add: 7-4

--- chapter_7/restaurant/src/lib.rs
+++ chapter_7/restaurant/src/lib.rs
@@ -2,20 +2,14 @@
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
-
-        fn sert_at_table() {}
     }
 
     mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-        fn cook_order() {}
-
+        fn _serve_order() {}
         mod test {
-            fn fix_incorrect_order() {
+            fn _fix_incorrect_order() {
                 // 相対パスで親モジュールにアクセス
-                super::serve_order();
+                super::_serve_order();
             }
         }
     }
@@ -24,14 +18,14 @@ mod front_of_house {
 mod back_of_house {
     pub struct Breakfast {
         pub toast: String,
-        seasonal_fruit: String,
+        _seasonal_fruit: String,
     }
 
     impl Breakfast {
         pub fn summer(toast: &str) -> Breakfast {
             Breakfast {
                 toast: String::from(toast),
-                seasonal_fruit: String::from("apple"),
+                _seasonal_fruit: String::from("apple"),
             }
         }
     }
@@ -42,12 +36,44 @@ mod back_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    // 絶対パス
-    crate::front_of_house::hosting::add_to_waitlist();
+//-------------------------------------
+// use でモジュールをスコープに持ち込む
+// pub use で外部に名前を公開（再公開）
+pub use crate::front_of_house::hosting;
+//-------------------------------------
 
-    // 相対パス
+//-------------------------------------
+// asキーワードでリネームする
+// use std::fmt::Result;
+// use std::fmt::Result as IoResult;
+//-------------------------------------
+
+//-------------------------------------
+// ネストしたパスで纏めて持ち込む
+
+// use std::cmp::Ordering;
+// use std::io;
+// ↓
+// use std::{cmp::Ordering, io};
+//-------------------------------------
+
+//-------------------------------------
+// 片方がもう片方のサブパスである場合
+
+// use std::io;
+// use std::io::Write;
+// ↓
+// use std::io::{self, Write};
+//-------------------------------------
+
+//-------------------------------------
+// glob演算子で全ての公開要素を持ち込む
+// use std::collections::*;
+//-------------------------------------
+
+pub fn eat_at_restaurant() {
     front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     //---------------------------------
 
@@ -62,6 +88,6 @@ pub fn eat_at_restaurant() {
     //---------------------------------
 
     // enumの列挙子を全て公開
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+    let _order1 = back_of_house::Appetizer::Soup;
+    let _order2 = back_of_house::Appetizer::Salad;
 }
