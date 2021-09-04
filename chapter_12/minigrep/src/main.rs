839444461eadaa5fa9e288a9aabe6c6ecd38f6d2
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-09-04 10:50:50 +0900

    clean

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -3,20 +3,31 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
+    // コマンドライン引数をパース
     let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
 
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("query = {}", query);
-    println!("filename = {}", filename);
+    println!("query = {}", config.query);
+    println!("filename = {}", config.filename);
 
     // ファイルを開く
-    let mut f = File::open(filename).expect("ファイルが見つかりません");
+    let mut f = File::open(config.filename).expect("ファイルが見つかりません");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("ファイルの読み込み中に問題がありました");
 
     println!("text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
