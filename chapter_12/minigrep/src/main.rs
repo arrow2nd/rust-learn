17b1088a6bdf1d37c6625ffcf67f30399e538491
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-09-06 21:34:05 +0900

    change: println! -> eprintln!

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     // コマンドライン引数をパース
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("引数の解析時に問題が発生しました: {}", err);
+        eprintln!("引数の解析時に問題が発生しました: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("アプリケーションエラー: {}", e);
+        eprintln!("アプリケーションエラー: {}", e);
         process::exit(1);
     }
 }
